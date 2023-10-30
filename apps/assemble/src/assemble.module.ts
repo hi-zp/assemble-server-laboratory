fb1cf5f101743e384df538acb0cd0f5e279057de
Coding.Zhu <coding.zhu@italki.com>
2023-10-30 15:42:45 +0800

    feat: global conf & swagger

--- apps/assemble/src/assemble.module.ts
+++ apps/assemble/src/assemble.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AssembleController } from './assemble.controller';
 import { AssembleService } from './assemble.service';
 import { NestConfigModule } from '@assemble/config';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { HelperService } from '@assemble/common';
 
 @Module({
-  imports: [NestConfigModule],
+  imports: [
+    NestConfigModule,
+    DevtoolsModule.register({
+      http: HelperService.isDev(),
+    }),
+  ],
   controllers: [AssembleController],
   providers: [AssembleService],
 })
--- apps/assemble/src/main.ts
+++ apps/assemble/src/main.ts
@@ -7,11 +7,20 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { Configs } from '@assemble/config';
 import { AppUtils, HelperService } from '@assemble/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { LoggerErrorInterceptor } from 'nestjs-pino';
+import { I18nValidationExceptionFilter } from 'nestjs-i18n';
+import { useContainer } from 'class-validator';
+import chalk from 'chalk';
+
+declare const module: { hot: { accept: () => void; dispose: (argument: () => Promise<void>) => void } };
+
+const logger = new Logger("Bootstrap");
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AssembleModule,
-    new FastifyAdapter(),
+    new FastifyAdapter()
   );
 
   const configService = app.get(ConfigService<Configs, true>);
@@ -22,6 +31,58 @@ async function bootstrap() {
 
   if (!HelperService.isProd()) AppUtils.setupSwagger(app, configService);
 
-  await app.listen(3000);
+  // =====================================================
+  // configure global pipes, filters, interceptors
+  // =====================================================
+
+  const globalPrefix = configService.get("app.prefix", { infer: true });
+
+  app.setGlobalPrefix(globalPrefix);
+
+  app.useGlobalPipes(new ValidationPipe(AppUtils.validationPipeOptions()));
+
+  app.useGlobalFilters(new I18nValidationExceptionFilter({ detailedErrors: false }));
+
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  // =========================================================
+  // configure shutdown hooks
+  // =========================================================
+
+  app.enableShutdownHooks();
+
+  useContainer(app.select(AssembleModule), { fallbackOnErrors: true });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  const port = process.env.PORT ?? configService.get("app.port", { infer: true })!;
+
+  await app.listen(port);
+
+  const appUrl = `http://localhost:${port}/${globalPrefix}`;
+
+  logger.log(`==========================================================`);
+  logger.log(`🚀 Application is running on: ${chalk.green(appUrl)}`);
+
+  logger.log(`==========================================================`);
+  logger.log(
+    `🚦 Accepting request only from: ${chalk.green(
+      `${configService.get("app.allowedOrigins", { infer: true }).toString()}`,
+    )}`,
+  );
+
+  if (!HelperService.isProd()) {
+    const swaggerUrl = `http://localhost:${port}/doc`;
+    logger.log(`==========================================================`);
+    logger.log(`📑 Swagger is running on: ${chalk.green(swaggerUrl)}`);
+  }
 }
-bootstrap();
+
+try {
+  (async () => await bootstrap())();
+} catch (error) {
+  logger.error(error);
+}
